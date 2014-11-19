abbb8eeb8c0591b5dbcd999337bca4568b7e6ff2
waselrosa <rosa@waselrosa.com>
2014-11-19 14:56:37 -0800

    users can see quotes on the homepage

--- Gemfile
+++ Gemfile
@@ -44,3 +44,9 @@ gem 'bootstrap-sass', '~> 3.3.1'
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+
+group :development do
+  gem 'spring'
+  gem "better_errors"
+  gem "binding_of_caller"
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+  def home
+    @quotes = ['"Failure is not an option. Everyone has to Succeed." - Arnold Schwarzenegger',
+    '"Your time is limited, so don\'t waste it living someone else\'s life." - Steve Jobs',
+    '"Better Ingredients, Better Pizza." - Papa John',
+    ]
+  end
+end
