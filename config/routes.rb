6b64d6585cd26940a79ba56a67ebaa3b99a6f9fe
waselrosa <rosa@waselrosa.com>
2014-12-02 13:58:39 -0800

    added path for tasks in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   get 'faq' => 'pages#faq'
 
+  get 'tasks' => 'tasks#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
