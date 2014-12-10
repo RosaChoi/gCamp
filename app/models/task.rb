3bdf4d0842619a55c4e1e02740f63f77254ead88
waselrosa <rosa@waselrosa.com>
2014-12-09 17:05:04 -0800

    Users cannot enter a date in the past when creating a task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,12 @@
 class Task < ActiveRecord::Base
   validates :description, presence:true
+
+  validate :due_in_future, on: :create
+
+  def due_in_future
+    if due_date < Date.today then
+      errors.add(:due_date, "cannot be in the past")
+    end
+  end
+
 end
